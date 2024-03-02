555a74f4ea179cc30378b1bef3b6160efa21a422
ddwolf <910063@gmail.com>
2024-03-02 16:18:39 +0800

    can fall now

--- Script/Border.gd
+++ Script/Border.gd
@@ -7,14 +7,17 @@ var BORDER_HEIGHT:int = 20
 var BORDER_WIDTH:int  = 10
 
 # variables
+@export var m_fallSpeed:float = 1
 var m_playfield = []
+var m_tetroThisTurn = []
 var m_tetrominoFactor = preload("Tetromino.gd").new()
 var m_startPos = { "x" = 3.5 * TetrominoDefine.BLOCK_WIDTH,  "y" = -(BORDER_HEIGHT - 0.5) * TetrominoDefine.BLOCK_HEIGHT }
+@onready var m_timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	init_variable()
-	m_tetrominoFactor.CreateRandomTetro()
+	getNextTetromino()
 	pass # Replace with function body.
 
 func init_variable():
@@ -28,6 +31,31 @@ func init_variable():
 	# 綁訂節點
 	m_tetrominoFactor.setBorderNode(self)
 	m_tetrominoFactor.setStartPos(m_startPos)
+	
+	# 落下時間
+	m_timer.wait_time = m_fallSpeed
+
+# 拿下一個方塊
+func getNextTetromino():
+	m_tetroThisTurn = m_tetrominoFactor.CreateRandomTetro()
+	print(m_tetroThisTurn)
+	# 開始定時執行
+	m_timer.start()
+	pass
+
+func fallTetrominoThisTurn():
+	var blockNode = null
+	for idx in range(m_tetroThisTurn.size()):
+		blockNode = m_tetroThisTurn[idx]
+		blockNode.position.y = blockNode.position.y + 64
+		pass
+	
+
+func _on_timer_timeout():
+	if m_tetroThisTurn.size() <= 0:
+		return
+	fallTetrominoThisTurn()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
--- Script/Tetromino.gd
+++ Script/Tetromino.gd
@@ -6,11 +6,9 @@ var BLOCK_FILE =  "res://Scenes/Game/Block.tscn"
 
 var m_borderNode = null
 var m_startPos = {"x":0, "y":0}
-var m_blockPool = []
 
 
-@onready var BLOCK =  preload("res://Scenes/Game/Block.tscn")
-@onready var RED_SPR = preload("res://Resource/img/Red.png")
+var BLOCK_PATH = "res://Scenes/Game/Block.tscn"
 @onready var UI_ROOT = $Node
 
 # Called when the node enters the scene tree for the first time.
@@ -24,41 +22,29 @@ func setStartPos(startPos:Dictionary):
 	m_startPos.x = startPos.x
 	m_startPos.y = startPos.y
 
-func CreateRandomTetro()->bool:
+func CreateRandomTetro()->Array:
 	if not m_borderNode:
-		return false
+		return []
 	
 	var ranblockDefine = TetrominoDefine.NORMAL_TETROMINO[ randi() % 7 ]
-	CreateTetromino(ranblockDefine)
-	return true
+	return CreateTetromino(ranblockDefine)
 	
 
-func CreateTetromino(tetrominoSetting:Dictionary):
+func CreateTetromino(tetrominoSetting:Dictionary)->Array:
+	var blockList = []
 	var blockPos = tetrominoSetting.cellsVector
+	
 	for col in range(blockPos.size()):
 		for row in range(blockPos[col].size()):
 			if blockPos[col][row] == 1:
-				print("found at col: %d row: %d" % [col, row])
-				print("position should be y x : %d %d" % [col * TetrominoDefine.BLOCK_HEIGHT, row  * TetrominoDefine.BLOCK_WIDTH])
-				var testSpr = Sprite2D.new()
-				testSpr.texture = load(tetrominoSetting["spr"])
-				testSpr.position.x = row * TetrominoDefine.BLOCK_WIDTH + m_startPos.x
-				testSpr.position.y = col * TetrominoDefine.BLOCK_HEIGHT + m_startPos.y
-				m_borderNode.add_child(testSpr)
+				var newBlock = load(BLOCK_PATH).instantiate()
+				newBlock.get_node("./Sprite2D").texture = load(tetrominoSetting["spr"])
+				newBlock.position.x = row * TetrominoDefine.BLOCK_WIDTH + m_startPos.x
+				newBlock.position.y = col * TetrominoDefine.BLOCK_HEIGHT + m_startPos.y
+				blockList.append(newBlock)
+				m_borderNode.add_child(newBlock)
 
-func ClearAllPiece():
-	for blockData in m_blockPool:
-		blockData["collision"].disabled = true
-		blockData["node"].visible = false
-	pass
-
-func CreateNewBlock():
-	var blockData = {}
-	blockData["node"] = load(BLOCK_FILE).instantiate()
-	blockData["spr"] = blockData.node.get_node("Sprite2D")
-	blockData["collision"] = blockData.node.get_node("CollisionShape2D")
-	
-	return blockData
+	return blockList
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
